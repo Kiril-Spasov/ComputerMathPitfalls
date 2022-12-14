e5042a363049d1e61c3816ca9e62356fd700d9c4
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-14 10:54:04 +0200

    Add project files.

---
+++ ComputerMathPitfalls/FormMathPitfalls.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ComputerMathPitfalls
+{
+    partial class FormMathPitfalls
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnProblem1 = new System.Windows.Forms.Button();
+            this.BtnProblem2 = new System.Windows.Forms.Button();
+            this.BtnProblem3 = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.BtnProblem4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnProblem1
+            // 
+            this.BtnProblem1.Location = new System.Drawing.Point(66, 52);
+            this.BtnProblem1.Name = "BtnProblem1";
+            this.BtnProblem1.Size = new System.Drawing.Size(164, 60);
+            this.BtnProblem1.TabIndex = 0;
+            this.BtnProblem1.Text = "Problem 1";
+            this.BtnProblem1.UseVisualStyleBackColor = true;
+            this.BtnProblem1.Click += new System.EventHandler(this.BtnProblem1_Click);
+            // 
+            // BtnProblem2
+            // 
+            this.BtnProblem2.Location = new System.Drawing.Point(67, 145);
+            this.BtnProblem2.Name = "BtnProblem2";
+            this.BtnProblem2.Size = new System.Drawing.Size(163, 59);
+            this.BtnProblem2.TabIndex = 1;
+            this.BtnProblem2.Text = "Problem 2";
+            this.BtnProblem2.UseVisualStyleBackColor = true;
+            this.BtnProblem2.Click += new System.EventHandler(this.BtnProblem2_Click);
+            // 
+            // BtnProblem3
+            // 
+            this.BtnProblem3.Location = new System.Drawing.Point(66, 239);
+            this.BtnProblem3.Name = "BtnProblem3";
+            this.BtnProblem3.Size = new System.Drawing.Size(162, 64);
+            this.BtnProblem3.TabIndex = 2;
+            this.BtnProblem3.Text = "Problem 3";
+            this.BtnProblem3.UseVisualStyleBackColor = true;
+            this.BtnProblem3.Click += new System.EventHandler(this.BtnProblem3_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(307, 52);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtResult.Size = new System.Drawing.Size(415, 352);
+            this.TxtResult.TabIndex = 3;
+            // 
+            // BtnProblem4
+            // 
+            this.BtnProblem4.Location = new System.Drawing.Point(66, 343);
+            this.BtnProblem4.Name = "BtnProblem4";
+            this.BtnProblem4.Size = new System.Drawing.Size(161, 61);
+            this.BtnProblem4.TabIndex = 4;
+            this.BtnProblem4.Text = "Problem 4";
+            this.BtnProblem4.UseVisualStyleBackColor = true;
+            this.BtnProblem4.Click += new System.EventHandler(this.BtnProblem4_Click);
+            // 
+            // FormMathPitfalls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnProblem4);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnProblem3);
+            this.Controls.Add(this.BtnProblem2);
+            this.Controls.Add(this.BtnProblem1);
+            this.Name = "FormMathPitfalls";
+            this.Text = "Math Pitfalls";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnProblem1;
+        private System.Windows.Forms.Button BtnProblem2;
+        private System.Windows.Forms.Button BtnProblem3;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Button BtnProblem4;
+    }
+}
+
---
+++ ComputerMathPitfalls/FormMathPitfalls.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ComputerMathPitfalls
+{
+    public partial class FormMathPitfalls : Form
+    {
+        public FormMathPitfalls()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnProblem1_Click(object sender, EventArgs e)
+        {
+            double x = -10;
+            double numerator;
+            double denumerator;
+            double y; 
+
+            while (x < 10)
+            {
+                
+                numerator = Math.Pow(x, 3) - 4 * Math.Pow(x, 2);
+                denumerator = Math.Pow(x, 2) - 7 * x + 12;
+
+                if (Math.Abs(denumerator) <= 0.00001)
+                {
+                    TxtResult.Text += "x = " + x.ToString("n2") + ", y is undefined - can't divide by 0" + Environment.NewLine;
+                }
+                else
+                {
+                    y = numerator / denumerator;
+                    TxtResult.Text += "x = " + x.ToString("n2") + ", y = " + y.ToString("n2") + Environment.NewLine;
+                }
+
+                x += 0.1;
+            }
+
+        }
+
+        private void BtnProblem2_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter side 'a'", "Side 'a'"));
+            double b = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter side 'b'", "Side 'b'"));
+            double c = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter side 'c'", "Side 'c'"));
+
+            if (Math.Abs(Math.Pow(c, 2) - Math.Pow(a, 2) - Math.Pow(b, 2)) <= 0.00001)
+            {
+                TxtResult.Text = "The triangle is right angled.";
+            }
+            else
+            {
+                TxtResult.Text = "The triangle is not right angled.";
+            }
+        }
+
+        private void BtnProblem3_Click(object sender, EventArgs e)
+        {
+            double x1 = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter point 'x1'", "Point 'x1'"));
+            double y1 = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter point 'y1'", "Point 'y1'"));
+            double x2 = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter point 'x2'", "Point 'x2'"));
+            double y2 = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter point 'y2'", "Point 'y2'"));
+            double x3 = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter point 'x3'", "Point 'x3'"));
+            double y3 = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter point 'y3'", "Point 'y3'"));
+
+            if (Math.Abs(((x2 - x1) / (y2 - y1)) - ((x3 - x1) / (y3 - y1))) <= 0.00001)
+            {
+                TxtResult.Text = "The points are collinear.";
+            }
+            else
+            {
+                TxtResult.Text = "The points are not collinear.";
+            }
+        }
+
+        private void BtnProblem4_Click(object sender, EventArgs e)
+        {
+
+            //Sqrt of 9 is 3,
+            //but sqrt of 10 is not 3, it's 3.1622...
+            //If we subtract the sqrt of the number with the int of the number, if it's a perfect sqrt it should be = 0.
+            //So sqrt of 9 is 3, - 3 is 0,
+            //and sqrt of 10 is 3.1622, - 3 is not 0.
+            for (double i = 1; i <= 1000; i++)
+            {
+                if (Math.Abs(Math.Sqrt(i) - (int)Math.Sqrt(i)) <= 0.00001)
+                {
+                    TxtResult.Text += i + " is a perfect square." + Environment.NewLine;
+                }
+                else
+                {
+                    TxtResult.Text += i + Environment.NewLine;
+                }
+            }
+        }
+    }
+}
